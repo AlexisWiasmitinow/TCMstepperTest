b865c6ed0e80c813513609e5347a7ca0c01f16f6
Alexis Wiasmitinow <wiasmitinow@gmail.com>
2023-04-23 20:28:11 +0200

    stallguard works

--- src/main.cpp
+++ src/main.cpp
@@ -15,122 +15,94 @@
  * + Speed up
  * - Slow down
  */
+// #include <SoftwareSerial.h>
+// #include <Streaming.h>
+#include <SpeedyStepper.h>
 #include <TMCStepper.h>
 
 #define MAX_SPEED 40 // In timer value
 #define MIN_SPEED 1000
 
-#define STALL_VALUE 100 // [0..255]
+#define STALL_VALUE 1 // [0..255]
 
-#define EN_PIN 4            // Enable
-#define DIR_PIN 16          // Direction
-#define STEP_PIN 19         // Step
-#define SW_RX 21            // TMC2208/TMC2224 SoftwareSerial receive pin
-#define SW_TX 22            // TMC2208/TMC2224 SoftwareSerial transmit pin
-#define SERIAL_PORT Serial1 // TMC2208/TMC2224 HardwareSerial port
+#define EN_PIN 15   // Enable
+#define DIR_PIN 19  // Direction
+#define STEP_PIN 21 // Step
+// #define RXD2 16             // TMC2208/TMC2224 SoftwareSerial receive pin
+// #define TXD2 17             // TMC2208/TMC2224 SoftwareSerial transmit pin
+#define SERIAL_PORT Serial2 // TMC2208/TMC2224 HardwareSerial port
 #define DRIVER_ADDRESS 0b00 // TMC2209 Driver address according to MS1 and MS2
+#define DIAG 4
+#define stepDelay 500
 
-#define R_SENSE 0.1f // Match to your driver
-                     // SilentStepStick series use 0.11
-                     // UltiMachine Einsy and Archim2 boards use 0.2
-                     // Panucatt BSD2660 uses 0.1
-                     // Watterott TMC5160 uses 0.075
-
-// Select your stepper driver type
-// TMC2209Stepper driver(&SERIAL_PORT, R_SENSE, DRIVER_ADDRESS);
-TMC2209Stepper driver(SW_RX, SW_TX, R_SENSE, DRIVER_ADDRESS);
-
-using namespace TMC2208_n;
-
-// Using direct register manipulation can reach faster stepping times
-#define STEP_PORT PORTF // Match with STEP_PIN
-#define STEP_BIT_POS 0  // Match with STEP_PIN
-
-ISR(TIMER1_COMPA_vect) {
-    // STEP_PORT ^= 1 << STEP_BIT_POS;
-    digitalWrite(STEP_PIN, !digitalRead(STEP_PIN));
-}
+#define R_SENSE 0.1f
+TMC2209Stepper driver(&SERIAL_PORT, R_SENSE, DRIVER_ADDRESS);
+SpeedyStepper stepper;
 
 void setup() {
-    Serial.begin(250000); // Init serial port and set baudrate
-    while (!Serial)
-        ; // Wait for serial port to connect
-    Serial.println("\nStart...");
-
-    SERIAL_PORT.begin(115200);
-    driver.beginSerial(115200);
 
     pinMode(EN_PIN, OUTPUT);
     pinMode(STEP_PIN, OUTPUT);
     pinMode(DIR_PIN, OUTPUT);
-    digitalWrite(EN_PIN, LOW);
+    pinMode(DIAG, INPUT);
+    // digitalWrite(EN_PIN, 0);
+    stepper.connectToPins(STEP_PIN, DIR_PIN);
+    // Serial.begin(250000); // Init serial port and set baudrate
+    Serial.begin(115200); // Init serial port and set baudrate
 
+    // SERIAL_PORT.begin(115200);
+    Serial2.begin(115200);
+    // SoftSerial.begin(115200);
+    //
+
+    while (!Serial)
+        ; // Wait for serial port to connect
     driver.begin();
-    driver.toff(4);
+    delay(1000);
+    Serial.println("\nStart...");
+
+    // driver.beginSerial(115200);
+
+    driver.toff(5);
     driver.blank_time(24);
-    driver.rms_current(400); // mA
-    driver.microsteps(16);
+    driver.rms_current(2400); // mA
+    // driver.rms_current(800); // mA
+    driver.microsteps(4);
+    driver.SGTHRS(STALL_VALUE);
+    driver.shaft(0);
+    driver.en_spreadCycle(false);
+    driver.pwm_autoscale(true);
+    driver.intpol(true);
+
+    stepper.setCurrentPositionInSteps(0);                   // Set zero position
+    stepper.setSpeedInStepsPerSecond(1000);                 // Set Speed
+    stepper.setAccelerationInStepsPerSecondPerSecond(1000); // Set acceleration, smaller value for super smooth direction changing
+
+    delay(500);
+    // driver.VACTUAL(2000);
+    // driver.VACTUAL(0);
+    Serial.print("microsteps: ");
+    uint16_t msread = driver.microsteps();
+    Serial.println(msread);
+    Serial.print("ma: ");
+    uint16_t maread = driver.rms_current();
+    Serial.println(maread);
+    // Serial2 << driver.VACTUAL() << endl;
     driver.TCOOLTHRS(0xFFFFF); // 20bit max
-    driver.semin(5);
+    driver.semin(0);
     driver.semax(2);
     driver.sedn(0b01);
-    driver.SGTHRS(STALL_VALUE);
-
-    // Set stepper interrupt
-    {
-        cli();       // stop interrupts
-        TCCR1A = 0;  // set entire TCCR1A register to 0
-        TCCR1B = 0;  // same for TCCR1B
-        TCNT1 = 0;   // initialize counter value to 0
-        OCR1A = 256; // = (16*10^6) / (1*1024) - 1 (must be <65536)
-        // turn on CTC mode
-        TCCR1B |= (1 << WGM12);
-        // Set CS11 bits for 8 prescaler
-        TCCR1B |= (1 << CS11); // | (1 << CS10);
-        // enable timer compare interrupt
-        TIMSK1 |= (1 << OCIE1A);
-        sei(); // allow interrupts
-    }
 }
 
 void loop() {
-    static uint32_t last_time = 0;
-    uint32_t ms = millis();
+    // driver.moveTo(100);
+    // stepper.moveToPositionInSteps(6400);
 
-    while (Serial.available() > 0) {
-        int8_t read_byte = Serial.read();
-#ifdef USING_TMC2660
-        if (read_byte == '0') {
-            TIMSK1 &= ~(1 << OCIE1A);
-            driver.toff(0);
-        } else if (read_byte == '1') {
-            TIMSK1 |= (1 << OCIE1A);
-            driver.toff(driver.savedToff());
-        }
-#else
-        if (read_byte == '0') {
-            TIMSK1 &= ~(1 << OCIE1A);
-            digitalWrite(EN_PIN, HIGH);
-        } else if (read_byte == '1') {
-            TIMSK1 |= (1 << OCIE1A);
-            digitalWrite(EN_PIN, LOW);
-        }
-#endif
-        else if (read_byte == '+') {
-            if (OCR1A > MAX_SPEED)
-                OCR1A -= 20;
-        } else if (read_byte == '-') {
-            if (OCR1A < MIN_SPEED)
-                OCR1A += 20;
-        }
-    }
-
-    if ((ms - last_time) > 100) { // run every 0.1s
-        last_time = ms;
-
-        Serial.print("0 ");
-        Serial.print(driver.SG_RESULT(), DEC);
-        Serial.print(" ");
-        Serial.println(driver.cs2rms(driver.cs_actual()), DEC);
-    }
+    digitalWrite(STEP_PIN, 1);
+    delayMicroseconds(stepDelay);
+    digitalWrite(STEP_PIN, 0);
+    delayMicroseconds(stepDelay);
+    // delay(500);
+    Serial.print("diag: ");
+    Serial.println(digitalRead(DIAG));
 }
