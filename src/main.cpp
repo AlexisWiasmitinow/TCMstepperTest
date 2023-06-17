a213291ad55af80f53f6766b68bc73f309987188
Alexis Wiasmitinow <wiasmitinow@gmail.com>
2023-06-17 08:48:57 +0200

    tweaked

--- src/main.cpp
+++ src/main.cpp
@@ -25,14 +25,14 @@
 
 #define STALL_VALUE 1 // [0..255]
 
-#define EN_PIN 15           // Enable
+#define EN_PIN 32           // Enable
 #define DIR_PIN 19          // Direction
-#define STEP_PIN 21         // Step
-#define RXD2 18             // TMC2208/TMC2224 SoftwareSerial receive pin
-#define TXD2 16             // TMC2208/TMC2224 SoftwareSerial transmit pin
+#define STEP_PIN 14         // Step
+#define RXD2 17             // TMC2208/TMC2224 SoftwareSerial receive pin
+#define TXD2 12             // TMC2208/TMC2224 SoftwareSerial transmit pin
 #define SERIAL_PORT Serial2 // TMC2208/TMC2224 HardwareSerial port
-#define DRIVER_ADDRESS 0b11 // TMC2209 Driver address according to MS1 and MS2 0bxy where x is MS2 and y MS1
-#define DIAG 4
+#define DRIVER_ADDRESS 0b00 // TMC2209 Driver address according to MS1 and MS2 0bxy where x is MS2 and y MS1
+#define DIAG 35
 #define stepDelay 500
 
 #define R_SENSE 0.1f
@@ -62,18 +62,21 @@ void setup() {
         ; // Wait for serial port to connect
     driver.begin();
     delay(1000);
-    Serial.println("\nStart...");
+    Serial.println("\nSetup...");
 
     // driver.beginSerial(115200);
 
-    driver.toff(5);
+    driver.toff(0);
     driver.blank_time(24);
-    driver.rms_current(2400); // mA
-    // driver.rms_current(800); // mA
+    // driver.rms_current(2400); // mA
+    driver.rms_current(400); // mA
     driver.microsteps(4);
     driver.SGTHRS(STALL_VALUE);
-    driver.shaft(0);
-    driver.en_spreadCycle(false);
+    bool clockwise = true;
+    int dir = clockwise;
+    driver.shaft(dir);
+    // driver.shaft(false);
+    driver.en_spreadCycle(false); // enables stealthchop and thus also stallguard
     driver.pwm_autoscale(true);
     driver.intpol(true);
 
@@ -82,15 +85,25 @@ void setup() {
     stepper.setAccelerationInStepsPerSecondPerSecond(10000); // Set acceleration, smaller value for super smooth direction changing
 
     delay(500);
+    driver.toff(5);
+    delay(500);
     // driver.VACTUAL(2000);
     // driver.VACTUAL(0);
     driver.TCOOLTHRS(0xFFFFF); // 20bit max
     driver.semin(0);
     driver.semax(2);
-    driver.sedn(0b01);
+    driver.sedn(1);
+    driver.toff(5);
+    driver.SGTHRS(STALL_VALUE);
+    // driver.TCOOLTHRS(STALL_VALUE); another stallguard threashold...
+    // https://www.youtube.com/watch?v=J3U3e2WiPLg
+    // https://valarsystems.com/blogs/val-2000/section-7-stallguard
+    Serial.println("\nStart...");
 }
 bool runMotor = true;
+bool startMotor = true;
 bool diag = false;
+int loopCounter = 0;
 void loop() {
     // driver.moveTo(100);
     // stepper.moveToPositionInSteps(6400);
@@ -99,7 +112,7 @@ void loop() {
         delayMicroseconds(stepDelay);
         digitalWrite(STEP_PIN, 0);
         // delay(500);
-        // diag = digitalRead(DIAG);
+        diag = digitalRead(DIAG);
         if (diag) {
             runMotor = false;
         }
@@ -107,5 +120,12 @@ void loop() {
         Serial.println(diag);
 
         delayMicroseconds(stepDelay);
+    } else if (loopCounter < 10) {
+        digitalWrite(STEP_PIN, 1);
+        delayMicroseconds(stepDelay);
+        digitalWrite(STEP_PIN, 0);
+        delayMicroseconds(stepDelay);
+        startMotor = false;
+        loopCounter++;
     }
 }
