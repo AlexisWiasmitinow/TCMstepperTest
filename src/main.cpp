94ac26df444233511378db482c67e9314d2dfe87
unknown <abraham.salom@adaire.com>
2024-02-21 01:01:12 +0200

    Direction change working, stall working in progress

--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,27 @@
 #include <TMCStepper.h>
 #include <SpeedyStepper.h> 
 
-#define DIR_PIN          2    // Direction
-#define STEP_PIN         4    // Step
-#define SERIAL_PORT Serial2   // HardwareSerial port 
-#define DRIVER_ADDRESS 0b00   // TMC2209 Driver address according to MS1 and MS2
-#define R_SENSE 0.11          // Match to your driver
+#define DIR_PIN          2 // Direction
+#define STEP_PIN         4 // Step
+#define SERIAL_PORT Serial2 // HardwareSerial port 
+#define DRIVER_ADDRESS 0b00 // TMC2209 Driver address according to MS1 and MS2
+#define R_SENSE 0.11        // Match to your driver
+#define STALL_VALUE 10      // Stall value
 
 TMC2209Stepper driver(&SERIAL_PORT, R_SENSE, DRIVER_ADDRESS);
 SpeedyStepper stepper;
 
-bool shaft = false;  // ONLY NEEDED FOR CHANGING DIRECTION VIA UART, NO NEED FOR DIR PIN FOR THIS
+bool shaft = false;       // ONLY NEEDED FOR CHANGING DIRECTION VIA UART, NO NEED FOR DIR PIN FOR THIS
+bool shaftPosition;
 uint16_t msread;
 uint16_t rms_current;
 uint16_t CurrentPosition;
+uint16_t StallValue;
 
 void setup() {
+  //pinMode(DIR_PIN, OUTPUT);
+  //pinMode(STEP_PIN,OUTPUT);
+  pinMode(18, OUTPUT);
   stepper.connectToPins(STEP_PIN, DIR_PIN); // INITIALIZE SpeedyStepper
 
   SERIAL_PORT.begin(115200);      // INITIALIZE UART TMC2209
@@ -24,12 +30,16 @@ void setup() {
   delay(500);
   Serial.println(F("Serial Initialized"));
 
-  driver.begin();                 // Initialize driver
-  driver.toff(5);                 // Enables driver in software
-  driver.rms_current(300);        // Set motor RMS current
-  driver.microsteps(16);          // Set microsteps to 1/2
-  driver.pwm_autoscale(true);     // Needed for stealthChop
-  // driver.en_spreadCycle(true); // Toggle spreadCycle for smooth & silent operation
+  driver.pdn_disable(true);         // Enable driver UART
+  driver.begin();                   // Initialize driver  
+  driver.toff(5);                   // Enables driver in software
+  driver.blank_time(24);            // Comparator blank time. This time needs to safely cover the switching
+  driver.rms_current(300);          // Set motor RMS current
+  driver.microsteps(1);             // Set microsteps to 1
+  driver.pwm_autoscale(true);       // Needed for stealthChop
+  driver.en_spreadCycle(false);     // Toggle spreadCycle for smooth & silent operation and enable stallgaurd 
+  driver.TCOOLTHRS(0xFFFFF);        // The stall output signal become enabled when exceeding this velocity, hence max speed   
+  driver.SGTHRS(STALL_VALUE);       // The stall output becomes active if SG_RESULT fall below this value.     
 
   stepper.setCurrentPositionInSteps(0);                   // Set zero position
   stepper.setSpeedInStepsPerSecond(400);                  //Set Speed
@@ -37,41 +47,31 @@ void setup() {
 }
 
 void loop() {
-
-  stepper.moveRelativeInSteps(1600);
+  stepper.moveRelativeInSteps(800);
+  //stepper.moveToPositionInSteps(1600);
   delay(2000);  
 
   msread = driver.microsteps();
   rms_current = driver.rms_current();
   //CurrentPosition = stepper.moveToPositionInSteps();
 
-  msread = driver.microsteps();
-  Serial.print(F("Read microsteps via UART to test UART receive : "));      // Test if UART is working
-  Serial.println(msread);                                                   // If working, sends microstep value, otherwise, default is 256
-  Serial.print("Check position: ");                       
-  Serial.println(CurrentPosition);
-
-
-  Serial.println(F("Move 6400 steps forward at 600ma"));
-  stepper.moveToPositionInSteps(6400);
-  driver.rms_current(600);
-  rms_current = driver.rms_current();                                     // Increase current and test
-  Serial.print(F("Read current current high: "));
-  Serial.println(rms_current);
-
-  driver.microsteps(4);           
+  driver.microsteps(2);           
   msread = driver.microsteps();
   Serial.print(F("Read microsteps: "));
   Serial.println(msread);
  
-  shaft = !shaft;               // REVERSE DIRECTION
-  driver.shaft(shaft);          // SET DIRECTION
-  delay(50);
+  shaft = !shaft;                           // Reverse direction
+  driver.shaft(shaft);                      // Set direction
+  shaftPosition = driver.shaft();
   Serial.print("Shaft direction: ");
-  Serial.println(shaft);
+  Serial.println(shaftPosition);            // Receive shaft direction from driver
+  //digitalWrite(18, shaft);
 
   driver.rms_current(500);
   rms_current = driver.rms_current();
   Serial.print(F("Read current current slow: "));
   Serial.println(rms_current);
+
+  Serial.print(F("Stall Guard value: "));
+  Serial.println(driver.SG_RESULT());                     // Recieve stallguard value from driver
 }
